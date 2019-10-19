757c4b13df7223aa4fdbf7a2bf1fd32968b9569c
Mathieu Gamache <mathg99@hotmail.com>
2019-10-19 11:54:23 -0400

    Hello World Game

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import { ScoreScene } from "./scoreScene";
+import { WelcomeScene } from "./welcomeScene";
+import { GameScene } from "./gameScene";
+import "phaser";
+
+const config: Phaser.Types.Core.GameConfig = {
+    title: "Cat Virus Game",
+    width: 800,
+    height: 600,
+    parent: "game",
+    scene: [WelcomeScene, GameScene, ScoreScene],
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: false,
+        },
+    },
+    backgroundColor: "#18216D",
+};
+
+export class CatVirusGame extends Phaser.Game {
+    constructor(config: Phaser.Types.Core.GameConfig) {
+        super(config);
+    }
+}
+
+window.onload = () => {
+    let game2 = new CatVirusGame(config);
+};
